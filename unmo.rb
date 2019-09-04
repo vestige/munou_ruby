9d0f187697b226c22b073b2c842fe1e78a7ef5b4
Makoto Yonezawa <vestige@vestige.local>
2019-09-04 20:32:32 +0900

    add emotion

--- unmo.rb
+++ unmo.rb
@@ -1,6 +1,44 @@
 require 'responder'
 require 'dictionary'
 
+
+class Emotion
+  MOOD_MIN = -15
+  MOOD_MAX = 15
+  MOOD_RECOVERY = 0.5
+
+  def initialize(dict)
+    @dictionary = dict
+    @mood = 0
+  end
+
+  def update(input)
+    @dictionary.pattern.each do |ptn_item|
+      if ptn_item.match(input)
+        adjust_mood(ptn_item.modify)
+        break
+      end
+    end
+
+    if @mood < 0
+      @mood += MOOD_RECOVERY
+    elsif @mood > 0
+      @mood -= MOOD_RECOVERY
+    end
+  end
+
+  def adjust_mood(val)
+    @mood += val
+    if @mood > MOOD_MAX
+      @mood = MOOD_MAX
+    elsif @mood < MOOD_MIN
+      @mood = MOOD_MIN
+    end
+  end
+
+  attr_reader :mood
+end
+
 class Unmo
   def initialize(name)
     @name = name
